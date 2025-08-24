4e1ea6ccf40dfbfb1de49e8aac87123d4bd61021
Nico <richard.nicolas44@gmail.com>
2025-08-24 12:09:14 +0200

    modif composant echec

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { EchecsComponent } from "../echecs/echecs.component";
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './projects.component.html',
-  imports: [EchecsComponent, RouterModule, CommonModule]
+  styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  showChess = false;
-
-  toggleChessGame(): void {
-    this.showChess = !this.showChess;
-  }
 }
